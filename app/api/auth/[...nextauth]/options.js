57aae4aeb94735717b7e8c53e25323f046d6860e
fitse01 <fitsumtafese01@gmail.com>
2024-10-08 03:06:52 +0300

    sign up option added

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -16,6 +16,32 @@ export const options = {
                     role :userRole,
                 };
             },
+            clientId:process.env.GITHUB_ID,
+            clientSecret:process.env.GITHUB_Secret,
+        }),
+        GoogleProviders({
+            profile(profile){
+                console.log("Google Profile " , profile);
+
+                return{
+                    ...profile,
+                    id:profile.sub,
+                    role :userRole,
+                };
+            },
+            clientId:process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_Secret,
         }),
     ],
+
+    callbacks :{
+        async jwt ({token , user}){
+            if(user) token.role = user.role;
+            return token;
+        },
+        async session({ session,token }){
+            if(session?.user) session.user.role = token.role;
+            return session;
+        }
+    }
 }
