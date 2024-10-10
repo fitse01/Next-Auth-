35219c48961b3d95b2668b96ad4e45e081d1bc5f
fitse01 <fitsumtafese01@gmail.com>
2024-10-10 04:06:32 +0300

    middleware added

--- middleware.js
+++ middleware.js
@@ -1,7 +1,20 @@
 import withAuth from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
+export default withAuth (
+    function middleware(req){   
+        console.log(req.nextUrl.pathname);
+        console.log(req.nextauth.token.role);
 
-export { default } from "next-auth/middleware";
+        if(req.nextUrl.pathname.startsWith("/CreateUser") && req.nextauth.token.role != "admin"){
+            return NextResponse.rewrite(new URL("/Denied" , req.url))
+        }
+    } ,{
+        callbacks :{
+            authorized : ({token}) => !!token
+        },
+    }   
+)
+// export { default } from "next-auth/middleware";   // used only for authentication not authorization 
 
 export const config = {matcher : ["/CreateUser"]};
