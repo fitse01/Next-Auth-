8f8e617f6438805721d964d1ba6ccda637825194
fitse01 <fitsumtafese01@gmail.com>
2024-10-11 04:48:50 +0300

    update form submition

--- app/(components)/UserForm.jsx
+++ app/(components)/UserForm.jsx
@@ -1,3 +1,20 @@
 "use client ";
 import { useRouter } from "next/navigation";
 import React ,{useState} from "react";
+
+const UserForm = ()=>{
+    const router = useRouter();
+    const [formData , setFormData] = useState({});
+    const [errorMessage , setErrorMessage] = useState("");
+
+    const handleChange = (e)=>{
+        const value = e.target.value;
+        const name = e.target.name;
+        
+        setFormData((prevState)=>({
+            ...prevState,
+            [name]:value
+        }))
+    }
+}
+export default UserForm;
