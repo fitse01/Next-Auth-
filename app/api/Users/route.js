13b4ec60ba335c0bcdd0cf1dec12067658ded6af
fitse01 <fitsumtafese01@gmail.com>
2024-10-11 06:06:28 +0300

    update form submition

--- app/api/Users/route.js
+++ app/api/Users/route.js
@@ -8,8 +8,18 @@ export async function POST() {
         const userData = body.formData;
 
         // confirm if the data exist 
+        if(!userData?.email || !userData.password){
+            return NextResponse.json({Error :"All Fields are Required " } , {status:400});
+        }
+
+        // check if the email is duplicate 
+        const duplicate = await User.findOne({email : userData.email}).lean().exec();
+        if(duplicate){
+            return NextResponse.json({Error :"Duplicate Email " } , {status:409});
+        }
+
     } catch (err) {
         console.log(err);
-        return NextResponse.json({Error :"Error" , err} , {status:500})
+        return NextResponse.json({Error :"Error" , err} , {status:500});
     };
 }
